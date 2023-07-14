0b7a32c071797bc9ccb85addedae3e2546333697
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-14 15:08:50 -0500

    fetch andy's toys

--- src/index.js
+++ src/index.js
@@ -12,4 +12,36 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+  .then(resp=>resp.json())
+  .then(data=>data.forEach(element=>createCard(element)))
 });
+
+function createCard(toy){
+  console.log(toy)
+  const cardDiv = document.createElement('div')
+  cardDiv.className = 'card'
+
+  const nameTag = document.createElement('h2')
+  nameTag.innerText = toy.name
+  cardDiv.appendChild(nameTag)
+
+  const image = document.createElement('img')
+  image.src = toy.image
+  image.className = 'toy-avatar'
+  cardDiv.appendChild(image)
+
+  const likes = document.createElement('p')
+  likes.innerText = `${toy.likes} Likes`
+  cardDiv.appendChild(likes)
+
+  const likeButton = document.createElement('button')
+  likeButton.className = 'like-btn'
+  likeButton.id = toy.id
+  likeButton.innerText = `like ❤️`
+  cardDiv.appendChild(likeButton)
+  
+  const toyContainer =document.getElementById('toy-collection')
+  toyContainer.appendChild(cardDiv)
+}
