6c54fbc08a61330322874ad82c1335f0f84e3d31
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-17 14:38:26 -0500

    challenge 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 let addToy = false;
+let idCounter = 0
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
+  const inputForm = document.querySelector('form')
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -15,11 +18,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fetch('http://localhost:3000/toys')
   .then(resp=>resp.json())
-  .then(data=>data.forEach(element=>createCard(element)))
+  .then(data=>data.forEach(element=>{
+    createCard(element)
+    idCounter++
+  }))
+
+  inputForm.addEventListener('submit',e => {
+    e.preventDefault()
+    const inputList = document.querySelectorAll('input.input-text')
+    idCounter++
+    const newToy = {name: inputList[0].value, image: inputList[1].value, likes: 0, id: idCounter}
+    createCard(newToy)
+
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        'name': newToy.name,
+        'image': newToy.image,
+        'likes': 0,
+      })
+    })
+    .then(resp=>resp.json())
+  })
 });
 
 function createCard(toy){
-  console.log(toy)
   const cardDiv = document.createElement('div')
   cardDiv.className = 'card'
 
@@ -41,7 +68,27 @@ function createCard(toy){
   likeButton.id = toy.id
   likeButton.innerText = `like ❤️`
   cardDiv.appendChild(likeButton)
-  
+  addEventListenerToLikeButton(toy, likeButton)
+
   const toyContainer =document.getElementById('toy-collection')
   toyContainer.appendChild(cardDiv)
+}
+
+function addEventListenerToLikeButton(toy, likeButton){
+  likeButton.addEventListener('click',e=>{
+    const newLikes = toy.likes +1
+    fetch(`http://localhost:3000/toys/${likeButton.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+       },
+      body: JSON.stringify({
+        'likes': newLikes
+      })
+    })
+    toy.likes = newLikes
+    const likesParagraph = document.querySelectorAll('.card')[likeButton.id-1].querySelector('p')
+    likesParagraph.innerText = `${newLikes} Likes`
+  })
 }
